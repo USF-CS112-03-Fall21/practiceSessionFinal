0c4a05ce0fb0ed609dffbd042591346f943e3735
Olga Karpenko <okarpenko@usfca.edu>
2021-12-08 12:01:44 -0800

    Updated recursion exercises.

--- src/main/java/problems/recursion/RecursionExamples.java
+++ src/main/java/problems/recursion/RecursionExamples.java
@@ -1,6 +1,21 @@
-package problems.strings;
+package problems.recursion;
 
-public class StringUtil {
+public class RecursionExamples {
+
+    /** Compute x to the power of n, recursively, using the formulas below */
+    public static double power(int x, int n) {
+        double result = 0;
+        // recursively compute x^n using the following recursive definition
+        // if n > 0 and is even, x^n = x^(n/2) * x^(n/2)
+        // for example, x^4 = x^2 * x^2
+        // if n > 0 and is odd, x^n = x * x^(n/2) * x^(n/2)
+        // For example, x^5 = x * x^2 * x^2
+        // if n < 0, throw an exception  - no need to handle it for this practice session
+
+        // n = 0 is the base case. What will you return in this case?
+
+        return result;
+    }
 
     /**
      * Write a recursive method that checks whether a given string contains a given substring.
@@ -17,10 +32,16 @@ public class StringUtil {
     }
 
     public static void main(String[] args) {
+        // Test the power method
+        System.out.println(power(3, 4));    // 81
+        System.out.println(power(2, 5));   // 32
+        
         // Test the contains method:
         String s = "Hello, world, hello!";
         System.out.println(solution.StringUtil.contains(s, "world")); // true
         System.out.println(solution.StringUtil.contains(s, "old")); // false
+
+
 
 
     }
